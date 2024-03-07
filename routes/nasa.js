0d13b3402cd3fe380da06576d6cff647a758b079
mixarik <mixarikhh@gmail.com>
2024-03-07 15:26:16 +0100

    simplified data about meteors

--- routes/nasa.js
+++ routes/nasa.js
@@ -8,6 +8,10 @@ const needle = require('needle');
 //Env vars
 const API_BASE_URL = "https://api.nasa.gov/neo/rest/v1/feed";
 const API_KEY_NAME = "api_key";
+const START_DATE = "start_date";
+const START_DATE_VALUE = '2015-09-07';
+const END_DATE = "end_date";
+const END_DATE_VALUE = "2015-09-08";
 const API_KEY_VALUE = "cSZhruTYTjIDDmpTmvbfozr6eVmc7rz7wncZwUQ0";
 
 
@@ -15,7 +19,9 @@ router.get("/", async (req, res) => {
 
     try {
         const params = new URLSearchParams({
-            [API_KEY_NAME]: API_KEY_VALUE
+            [API_KEY_NAME]: API_KEY_VALUE,
+            [START_DATE]:START_DATE_VALUE,
+            [END_DATE]:END_DATE_VALUE
         });
         const apiRes = await needle('get', `${API_BASE_URL}?${params}`);
         const data = apiRes.body.near_earth_objects;
@@ -27,11 +33,9 @@ router.get("/", async (req, res) => {
                     estimated_diameter_meters: object.estimated_diameter.meters,
                     is_potentially_hazardous_asteroid: object.is_potentially_hazardous_asteroid,
                     close_approach_date_full: object.close_approach_data
-                        .map(el => el.close_approach_date_full)
-                        .join(''),
+                        .map(el => el.close_approach_date_full),
                     relative_velocity_kilometers_per_second: object.close_approach_data
                         .map(el => el.relative_velocity.kilometers_per_second)
-                        .join('')
                 }
             });
             return acc
